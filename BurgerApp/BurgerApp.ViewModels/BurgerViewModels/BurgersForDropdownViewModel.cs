0983d196cd8eef34c3de138542da67be957e23e1
cheppymk <toso.micevski@gmail.com>
2023-08-05 12:49:49 +0200

    1

--- BurgerApp/BurgerApp.ViewModels/BurgerViewModels/BurgersForDropdownViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BurgerApp.ViewModels.BurgerViewModels
-{
-    public class BurgersForDropdownViewModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
--- BurgerApp/BurgerApp.ViewModels/HomeViewModels/HomeViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BurgerApp.ViewModels.HomeViewModels
-{
-    public class HomeViewModel
-    {
-        public string BurgerForPromotion { get; set; }
-    }
-}
