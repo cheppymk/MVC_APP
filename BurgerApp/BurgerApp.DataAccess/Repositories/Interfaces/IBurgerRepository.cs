dda317939cf89a2fc3b6b34523bf7e8374ef1394
cheppymk <toso.micevski@gmail.com>
2023-08-05 12:47:21 +0200

    BurgerApp

--- BurgerApp/BurgerApp.DataAccess/Repositories/Interfaces/IBurgerRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BurgerApp.Domain.Models;
-
-namespace BurgerApp.DataAccess.Repositories.Interfaces
-{
-    public interface IBurgerRepository : IRepository<Burger>
-    {
-        Burger GetBurgerForPromotion();
-    }
-}
--- BurgerApp/BurgerApp.ViewModels/LocationViewModels/LocationsForDropdownViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BurgerApp.ViewModels.LocationViewModels
-{
-    public class LocationsForDropdownViewModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
